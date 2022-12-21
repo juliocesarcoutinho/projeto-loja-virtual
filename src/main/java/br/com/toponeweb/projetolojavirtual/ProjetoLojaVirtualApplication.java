b09f2d25f43e774b9ba4953a29784ffa992e721f
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2022-12-21 20:46:47 -0300

    Initial commit

---
+++ src/main/java/br/com/toponeweb/projetolojavirtual/ProjetoLojaVirtualApplication.java
@@ -0,0 +1,13 @@
+package br.com.toponeweb.projetolojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLojaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLojaVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/toponeweb/projetolojavirtual/ProjetoLojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.toponeweb.projetolojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLojaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
