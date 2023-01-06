db482efe0da1d9add275f843c56d76e33c445cae
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-01-06 17:42:29 -0300

    Criação da Entidade Conta A Receber e Criação do Enum pra Status

---
+++ src/main/java/br/com/toponeweb/projetolojavirtual/model/ContaReceber.java
@@ -0,0 +1,51 @@
+package br.com.toponeweb.projetolojavirtual.model;
+
+import br.com.toponeweb.projetolojavirtual.enums.StatusContaReceber;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "conta_receber")
+@SequenceGenerator(name = "seq_conta_receber", sequenceName = "seq_conta_receber", allocationSize = 1, initialValue = 1)
+public class ContaReceber implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_receber")
+    private Long id;
+
+    private String descricao;
+    @Enumerated(EnumType.STRING)
+    private StatusContaReceber status;
+
+    @Temporal(TemporalType.DATE)
+    private Date dtVencimento;
+    @Temporal(TemporalType.DATE)
+    private Date dtPagamento;
+
+    private BigDecimal valorTotal;
+    private BigDecimal valorDesconto;
+
+    @ManyToOne(targetEntity = Pessoa.class) /* Muitas contas para uma pessoa*/
+    @JoinColumn(name = "pessoa_id", nullable = false,foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ContaReceber that)) return false;
+
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
