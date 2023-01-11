b533c7446759a0a6707b3fb17a88a8b885547a9e
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-01-10 21:58:04 -0300

    Criação da entidade Nota Fiscal de compra

---
+++ src/main/java/br/com/toponeweb/projetolojavirtual/model/NotaFiscalCompra.java
@@ -0,0 +1,50 @@
+package br.com.toponeweb.projetolojavirtual.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "nota_fiscal_compra")
+@SequenceGenerator(name = "seq_nota_fiscal_compra", sequenceName = "seq_nota_fiscal_compra", allocationSize = 1, initialValue = 1)
+public class NotaFiscalCompra implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_fiscal_compra")
+    private Long id;
+
+    private String numeroNota;
+    private String serieNota;
+    private String descricaoObs;
+    private BigDecimal valorTotal;
+    private BigDecimal valorDesconto;
+    private BigDecimal valorIcms;
+
+    @Temporal(TemporalType.DATE)
+    private Date dataCompra;
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pesssoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private Pessoa pessoa;
+    @ManyToOne
+    @JoinColumn(name = "conta_pagar_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "conta_pagar_fk"))
+    private ContaPagar contaPagar;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NotaFiscalCompra that)) return false;
+
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
+}
