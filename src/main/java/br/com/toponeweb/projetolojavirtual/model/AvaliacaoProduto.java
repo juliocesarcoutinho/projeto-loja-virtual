5fb512509bfbfd570b5bb65fdcc7670e37a37161
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2023-01-11 16:22:21 -0300

    Criação da entidade Avaliação Produto

---
+++ src/main/java/br/com/toponeweb/projetolojavirtual/model/AvaliacaoProduto.java
@@ -0,0 +1,44 @@
+package br.com.toponeweb.projetolojavirtual.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "avaliacao_produto")
+@SequenceGenerator(name = "seq_avaliacao_produto", sequenceName = "seq_avaliacao_produto", allocationSize = 1, initialValue = 1)
+public class AvaliacaoProduto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_avaliacao_produto")
+    private Long id;
+
+    private String descricao;
+
+    private Integer nota;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name ="pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+    @ManyToOne
+    @JoinColumn(name ="produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
+    private Produto produto;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AvaliacaoProduto that)) return false;
+
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
+}
